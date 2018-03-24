cea7573466ed1087ae6b4fd648c56ae9db3b6bc2
Lakraa Omer <omer.lakraa@etud.u-picardie.fr>
2018-03-24 20:09:05 +0100

    héritage Object

--- Programmes/Object.js
+++ Programmes/Object.js
@@ -1,9 +1,8 @@
 function Object(){
-    
+
     var geometry = new THREE.CubeGeometry(2,2,4)
-    var material = new THREE.MeshBasicMaterial( {color: 0xA22800} );
+    var material = new THREE.MeshBasicMaterial( {color: 0xA22800} )
     this.__proto__ = new THREE.Mesh(geometry, material)
-    this.cube.position.set(0, 0, 0)
-    this.add(this.cube);
+
     console.log(this)
 }
