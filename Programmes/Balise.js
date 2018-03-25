ac0c718dc53a6c2cf1522ca3f91e534378ac3f95
Lakraa Omer <omer.lakraa@etud.u-picardie.fr>
2018-03-25 15:28:28 +0200

    modification taille

--- Programmes/Balise.js
+++ Programmes/Balise.js
@@ -1,8 +1,9 @@
 function Balise(){
 
-    var geometry = new THREE.CubeGeometry(2,4,2)
+    var geometry = new THREE.CubeGeometry(2, 2, 2)
     var material = new THREE.MeshBasicMaterial( {color: 0xFF0000} );
     this.__proto__ = new THREE.Mesh(geometry, material)
-
+    
+    this.visible = false
     console.log(this)
 }
