95f25314dcd315073703f357af23246952e569db
Lakraa Omer <omer.lakraa@etud.u-picardie.fr>
2018-04-01 15:10:34 +0200

    Changement organisation des fichiers

---
+++ js/Balise.js
@@ -0,0 +1,22 @@
+function Balise(tailleX, tailleY, text){
+    this.text = text
+    this.tailleX = tailleX
+    this.tailleY = tailleY
+    var geometry = new THREE.CubeGeometry(this.tailleX,this.tailleY , 0.01)
+    var material = new THREE.MeshBasicMaterial( {color: 0x635900} )
+    this.__proto__ = new THREE.Mesh(geometry, material)
+    this.position.z = -1
+    this.actif = false
+
+    this.light = new THREE.PointLight(0xF1DA00, 0.5, 20)
+    this.add(this.light)
+
+    this.isCollision = function(position){
+        if(position.x < this.position.x + tailleX/2 && position.x > this.position.x - this.tailleX/2 && position.y < this.position.y + tailleY/2 && position.y > this.position.y - this.tailleY/2 ){
+            return true
+        }else{
+            return false
+        }
+    }
+    console.log(this)
+}
---
+++ js/Panel.js
@@ -0,0 +1,41 @@
+function Panel(text, height, width, resolution, police){
+    
+    this.police = police
+    this.px = resolution
+    this.canvas = document.createElement("canvas")
+    this.contextCanvas = this.canvas.getContext("2d")
+    this.canvas.height = height * this.px
+    this.canvas.width = width * this.px
+    this.contextCanvas.shadowColor = "#000"
+    this.contextCanvas.shadowBlur = 7
+    this.background =  '#252000'
+    this.color = 'black'
+    this.text = text
+    this.textDefault = text
+
+    this.setText = function(text){
+        //Fond
+        this.contextCanvas.fillStyle = this.background
+        this.contextCanvas.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+        //Text
+        this.contextCanvas.fillStyle = this.color
+        this.contextCanvas.font = (this.px*this.police)+"pt arial bold"
+
+        material.map.needsUpdate = true
+        addMultiLineText("\n"+text, this.px*this.police, this.px*this.police, this.px*this.police, this.canvas.width - this.px*this.police, this.contextCanvas);
+    }
+    
+    var geometry = new THREE.PlaneGeometry(width/10, height/10, 1)
+    var material = new THREE.MeshBasicMaterial({ 
+        map: new THREE.Texture(this.canvas), 
+        transparent: false,
+        //color: 0x00FF00,
+        //side : THREE.DoubleSide
+    })
+    this.setText(text)
+    this.__proto__ = new THREE.Mesh(geometry, material)
+    this.doubleSided = true
+    this.position.set(0, 0, 0)
+    
+}
